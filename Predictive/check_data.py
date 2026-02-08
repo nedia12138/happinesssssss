5a7f3b5529ae63b63fc388da98fd52edba0fc64b
nedia12138 <gey616808@gmail.com>
2026-02-08 12:16:38 +0800

    修改数据库密码

--- Predictive/check_data.py
+++ Predictive/check_data.py
@@ -5,7 +5,7 @@ DB_CONFIG = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
-    'password': '123456',
+    'password': '12121212',
     'database': '0_80123xingfuganwajue',
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
